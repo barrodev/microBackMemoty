fe94c4779ca8655a8c725adecb01fa484d815b9c
barrobaye <bayemamourbarro01@gmail.com>
2024-02-08 21:00:04 +0000

    yet

---
+++ my-services/src/main/java/com/example/myservices/MyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyServicesApplication.class, args);
+    }
+
+}
